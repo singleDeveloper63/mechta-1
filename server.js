730bc209c1d9766fffd0d38e8d933fc4a62212f9
singleDeveloper63 <63005771+singleDeveloper63@users.noreply.github.com>
2020-07-25 09:05:00 +0300

    Update server.js
    Bug fixed

--- server.js
+++ server.js
@@ -29,6 +29,12 @@ app.get('/', function(req, res){
     res.send("Hello Server");
 });
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
 app.listen(PORT, function () {
     console.log('Server running ' + PORT);
 })
